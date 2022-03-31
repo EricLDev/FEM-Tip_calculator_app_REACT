076c981c73bd4d84e266726d9c4634343fcd73e9
Eric Lehmann <lehm_eric@yahoo.fr>
2022-03-31 19:16:24 +0200

    init project + components structure

---
+++ src/components/Calculator.js
@@ -0,0 +1,8 @@
+import React from "react";
+import classes from "./Calculator.module.css";
+
+const Calculator = () => {
+	return <div className={classes.calculator}>Calculator</div>;
+};
+
+export default Calculator;
---
+++ src/components/ResultCard.js
@@ -0,0 +1,8 @@
+import React from "react";
+import classes from "./ResultCard.module.css";
+
+const ResultCard = () => {
+	return <div>ResultCard</div>;
+};
+
+export default ResultCard;
